541e4bed346d89a526689267204f11740fc16ffa
Wesley van Schaijk <wschaijk@outlook.com>
2024-09-24 14:49:01 +0200

    Replaced Web Application with Class Library as starting point

---
+++ OpenRevisorCore/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace OpenRevisorCore
+
+
+module Questions =
+    
+    /// Represents a question with a text and an answer.
+    type Question = { Text: string; Answer: string }
+
+    /// <summary>
+    /// Checks if the given answer is correct for the given question.
+    /// </summary>
+    /// <param name="question">The question that is being answered.</param>
+    /// <param name="givenAnswer">The answer that needs to be checked.</param>
+    /// <returns>True if the answer matches, false otherwise.</returns>
+    let isCorrect (question: Question) (givenAnswer: string) =
+        question.Answer.Equals(givenAnswer, System.StringComparison.OrdinalIgnoreCase)
